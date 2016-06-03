13fd8fb0df3972df579b904c47c8036bd524a78d
Frankkiv <lufrankkiv@gmail.com>
2016-06-03 03:40:55 +0000

    FACE DIFF edit

--- face_detect.py
+++ face_detect.py
@@ -1,36 +1,111 @@
-import io
-import picamera
+# USAGE
+# python pi_surveillance.py --conf conf.json
+
+# import the necessary packages
+from pyimagesearch.tempimage import TempImage
+from dropbox.client import DropboxOAuth2FlowNoRedirect
+from dropbox.client import DropboxClient
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import argparse
+import warnings
+import datetime
+import imutils
+import json
+import time
 import cv2
-import numpy
 
-#Create a memory stream so photos doesn't need to be saved in a file
-stream = io.BytesIO()
+def diff(K, L):
+    if len(K) != len(L):
+        return 0
+    result = sum(abs(i[0]-i[1]) for i in zip(K, L))
+    return (sum(n) for n in result)
 
-#Get the picture (low resolution, so it should be quite fast)
-#Here you can also specify other parameters (e.g.:rotate the image)
-with picamera.PiCamera() as camera:
-    camera.resolution = (320, 240)
-    camera.capture(stream, format='jpeg')
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--conf", required=True,
+	help="path to the JSON configuration file")
+args = vars(ap.parse_args())
 
-#Convert the picture into a numpy array
-buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
+# filter warnings, load the configuration and initialize the Dropbox
+# client
+warnings.filterwarnings("ignore")
+conf = json.load(open(args["conf"]))
+client = None
 
-#Now creates an OpenCV image
-image = cv2.imdecode(buff, 1)
+# check to see if the Dropbox should be used
+if conf["use_dropbox"]:
+	# connect to dropbox and start the session authorization process
+	flow = DropboxOAuth2FlowNoRedirect(conf["dropbox_key"], conf["dropbox_secret"])
+	print "[INFO] Authorize this application: {}".format(flow.start())
+	authCode = raw_input("Enter auth code here: ").strip()
 
-#Load a cascade file for detecting faces
-face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
+	# finish the authorization and grab the Dropbox client
+	(accessToken, userID) = flow.finish(authCode)
+	client = DropboxClient(accessToken)
+	print "[SUCCESS] dropbox account linked"
 
-#Convert to grayscale
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = tuple(conf["resolution"])
+camera.framerate = conf["fps"]
+rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
 
-#Look for faces in the image using the loaded cascade file
-faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-print "Found "+str(len(faces))+" face(s)"
+# allow the camera to warmup, then initialize the average frame, last
+# uploaded timestamp, and frame motion counter
+print "[INFO] warming up..."
+time.sleep(conf["camera_warmup_time"])
+avg = None
+lastUploaded = datetime.datetime.now()
+Counter = 0
+lastFaces = ()
 
-#Draw a rectangle around every found face
-for (x,y,w,h) in faces:
-    cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+# capture frames from the camera
+for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image and initialize
+	# the timestamp and occupied/unoccupied text
+	image = f.array
+	timestamp = datetime.datetime.now()
 
-#Save the result image
-cv2.imwrite('result.jpg',image)
+	#Now creates an OpenCV image
+	#image = cv2.imdecode(buff, 1)
+
+	#Load a cascade file for detecting faces
+	face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.0.0/data/haarcascades/haarcascade_frontalface_alt.xml')
+
+	#Convert to grayscale
+	gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+	#Look for faces in the image using the loaded cascade file
+	faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+	print "Found "+str(Counter)+" face(s)"
+	print faces
+        print lastFaces
+
+	#Draw a rectangle around every found face
+	for (x,y,w,h) in faces:
+    	        cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+                if diff(faces, lastFaces) >= 150:
+                    Counter+=1
+                lastFaces = faces
+
+	#Save the result image
+	#cv2.imwrite('result.jpg',image)
+
+	# check to see if the frames should be displayed to screen
+	if conf["show_video"]:
+		# display the security feed
+		cv2.imshow("Face Detect", image)
+		key = cv2.waitKey(1) & 0xFF
+
+		# if the `q` key is pressed, break from the lop
+		if key == ord("q"):
+			break
+
+        # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+
+def dot(K, L):
+    if len(K) != len(L):
+        return 0
+    return sum(i[0]*i[1] for i in zip(K, L))
