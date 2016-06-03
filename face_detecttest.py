4fd705837270c1a24b07f43531fa83ef5e24a6fe
frankkivLU <frankkiv@31-34-120.wireless.csail.mit.edu>
2016-06-02 20:55:23 -0400

    add test

---
+++ face_detecttest.py
@@ -0,0 +1,94 @@
+# USAGE
+# python pi_surveillance.py --conf conf.json
+
+# import the necessary packages
+from pyimagesearch.tempimage import TempImage
+from dropbox.client import DropboxOAuth2FlowNoRedirect
+from dropbox.client import DropboxClient
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import argparse
+import warnings
+import datetime
+import imutils
+import json
+import time
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--conf", required=True,
+	help="path to the JSON configuration file")
+args = vars(ap.parse_args())
+
+# filter warnings, load the configuration and initialize the Dropbox
+# client
+warnings.filterwarnings("ignore")
+conf = json.load(open(args["conf"]))
+client = None
+
+# check to see if the Dropbox should be used
+if conf["use_dropbox"]:
+	# connect to dropbox and start the session authorization process
+	flow = DropboxOAuth2FlowNoRedirect(conf["dropbox_key"], conf["dropbox_secret"])
+	print "[INFO] Authorize this application: {}".format(flow.start())
+	authCode = raw_input("Enter auth code here: ").strip()
+
+	# finish the authorization and grab the Dropbox client
+	(accessToken, userID) = flow.finish(authCode)
+	client = DropboxClient(accessToken)
+	print "[SUCCESS] dropbox account linked"
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = tuple(conf["resolution"])
+camera.framerate = conf["fps"]
+rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
+
+# allow the camera to warmup, then initialize the average frame, last
+# uploaded timestamp, and frame motion counter
+print "[INFO] warming up..."
+time.sleep(conf["camera_warmup_time"])
+avg = None
+lastUploaded = datetime.datetime.now()
+motionCounter = 0
+
+# capture frames from the camera
+for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# grab the raw NumPy array representing the image and initialize
+	# the timestamp and occupied/unoccupied text
+	image = f.array
+	timestamp = datetime.datetime.now()
+
+	#Now creates an OpenCV image
+	#image = cv2.imdecode(buff, 1)
+
+	#Load a cascade file for detecting faces
+	face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.0.0/data/haarcascades/haarcascade_frontalface_alt.xml')
+
+	#Convert to grayscale
+	gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+	#Look for faces in the image using the loaded cascade file
+	faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+	print "Found "+str(len(faces))+" face(s)"
+
+	#Draw a rectangle around every found face
+	for (x,y,w,h) in faces:
+    	cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+
+	#Save the result image
+	#cv2.imwrite('result.jpg',image)
+
+	# check to see if the frames should be displayed to screen
+	if conf["show_video"]:
+		# display the security feed
+		cv2.imshow("Face Detect", frame)
+		key = cv2.waitKey(1) & 0xFF
+
+		# if the `q` key is pressed, break from the lop
+		if key == ord("q"):
+			break
+
+# clear the stream in preparation for the next frame
+rawCapture.truncate(0)
